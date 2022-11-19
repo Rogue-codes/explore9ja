9c452a0de8f371208455ec939fb69171c20075a1
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-11-19 22:29:23 +0100

    api integration

--- src/redux/TourApi.js
+++ src/redux/TourApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const tourApi = createApi({
   reducerPath: 'tourApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '//localhost:5000/api/v1/9ja-tours' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://explore9ja.onrender.com/api/v1/9ja-tours' }),
   endpoints: (builder) => ({
     getAllTours: builder.query({
       query: () => "/tours",
